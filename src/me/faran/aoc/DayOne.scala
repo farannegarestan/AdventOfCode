ce3216804f36a33e6a8203ad1a88be7aa8057849
Faran Negarestan <faran@faran.me>
2016-12-12 20:36:11 -0500

    using fold instead of recursion

--- src/me/faran/aoc/DayOne.scala
+++ src/me/faran/aoc/DayOne.scala
@@ -15,37 +15,30 @@ object DayOne {
       "L4", "L1", "R4", "R4", "R5", "R1", "L4", "R2", "L4", "L2", "L4", "R2", "L4", "L1", "L2", "R1", 
       "R4", "R3", "R2", "R2", "R5", "L1", "L2")
   def main(args: Array[String]) {
-    val final_coordinate = move((0,0), input, "N")
-    println(final_coordinate)
-  } 
-  
-  def move(coordinate: Tuple2[Int, Int], input: List[String], direction: String): Tuple2[Int, Int] = {
-    if (input.isEmpty)
-      coordinate
-    else {
-      val next = input.head
+    val final_location = input.foldLeft((0,0,"N")) { (coordinate, next) => 
       val d =  next.drop(1).toInt
-      val (x, y) = coordinate
+      val (x, y, direction) = coordinate
       if (next.take(1) == "R") {
         if (direction == "N")
-          move(Tuple2(x + d, y), input.tail, "E")
+          (x + d, y, "E")
         else if (direction == "E")
-          move(Tuple2(x , y - d), input.tail, "S")
+          (x , y - d, "S")
         else if (direction == "S")
-          move(Tuple2(x - d , y ), input.tail, "W")
+          (x - d , y, "W")
         else
-          move(Tuple2(x , y + d ), input.tail, "N")
+          (x , y + d, "N")
       } else {
         if (direction == "N")
-          move(Tuple2(x - d, y), input.tail, "W")
+          (x - d, y, "W")
         else if (direction == "E")
-          move(Tuple2(x , y + d), input.tail, "N")
+          (x , y + d, "N")
         else if (direction == "S")
-          move(Tuple2(x + d , y ), input.tail, "E")
+          (x + d , y, "E")
         else
-          move(Tuple2(x , y - d ), input.tail, "S")
+          (x , y - d, "S")
       }
     }
+    println(final_location)
   }
 }
 
